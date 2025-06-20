56c16be66bea93a5ebf5b5744574c09244cf2a4b
Páblisson Araújo <pablissonaraujo@gmail.com>
2025-06-20 16:00:33 -0300

    feat: criação da repository

--- internal/repository/account_repository.go
+++ internal/repository/account_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"time"
 
 	"github.com/pablisson/go-gateway/internal/domain"
 )
@@ -39,4 +40,34 @@ func (r *AccountRepository) Save(account *domain.Account) error {
 	}
 
 	return nil
+}
+
+func (r *AccountRepository) FindByAPIKey(apiKey string) (*domain.Account, error) {
+	var account domain.Account
+	var createdAt, updatedAt time.Time
+
+	err := r.db.QueryRow(`
+		SELECT id, name, email, api_key, balance, created_at, updated_at
+		FROM accounts
+		WHERE api_key = $1
+	`, apiKey).Scan(
+		&account.ID, 
+		&account.Name, 
+		&account.Email, 
+		&account.APIKey, 
+		&account.Balance, 
+		&account.CreatedAt, 
+		&account.UpdatedAt)
+
+		if err == sql.ErrNoRows {
+			return nil, domain.ErrAccountNotFound
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		account.CreatedAt = createdAt
+		account.UpdatedAt = updatedAt
+		return &account, nil
 }
